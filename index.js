813b9fc2b835b6227efd1f28e4cc7eb8872771b6
Oboite Lilian <129099786+CupOfCoco@users.noreply.github.com>
2023-09-13 13:00:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-times');
     linkUp.classList.toggle ('active');
 };
+let typingText = new Typed ("#text", {
+    strings: ['WEB DEVELOPER','FRONTEND DEVELOPER','WEB APP DESIGNER','WEB APP DEVELOPER'],
+    typeSpeed: 35,
+    startDelay:500,
+    backSpeed: 35,
+    backDelay: 1000,
+    loop: true,
+    showCursor:true
+});
+
+// ScrollReveal({
+//     reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay:200
+// });
+// ScrollReveal().reveal('.itemmenu , .heading' , {origin: 'top'});
+
